0a2a92812fe8df009f87dbe22c5ce4c5c8dc7576
buenosisabella <coreamilisabella@gmail.com>
2021-06-22 18:44:16 -0300

    realizado

---
+++ script.js
@@ -0,0 +1,12 @@
+
+// DESAFIO 05
+
+let five = ['maça', 'Mogango', 'banana', 'limao', 'maracujar'];
+console.log(five);
+five.unshift('chocolate');
+five.pop();
+five.push('manteiga', 'regueijão');
+five.shift();
+
+let numbers = [7,5,6,3,8,9,2,1,4];
+numbers.sort();
